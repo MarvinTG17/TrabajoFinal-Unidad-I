8596aa4492f89ced55a22b6b97ad782427300e7d
Marvin <taipe.marvin17@gmail.com>
2022-11-03 22:40:25 -0500

    menu principal tarea1

--- tarea1.py
+++ tarea1.py
@@ -1,6 +1,18 @@
-#TAREA 1 - marvin
-#TAREA 1
-print("Hola")
-#TAREA 1
-print("Hola")
-#TAREA 1 - marvin
+def menu_principal():
+    print("\n--------MENU DE BIENVENIDA--------")
+    print("\nEscoga un numero del menu:")
+    print("Opcion 1 : Leer archivo")
+    print("Opcion 2 : Listar libro")
+    print("Opcion 3 : Agregar libro")
+    print("Opcion 4 : Eliminar libro")
+    print("Opcion 5 : Buscar libro por ISBN o por título.")
+    print("Opcion 6 : Ordenar libros por titulo")
+    print("Opcion 7 : Buscar libros por autor, editorial o género.")
+    print("Opción 8 : Buscar libros por número de autores.")
+    print("Opcion 9 : Editar o actualizar datos de un libro")
+    print("Opcion 10: Guardar libros en archivo.")
+    print("Opcion 0 : Salir")
+
+    opcion = input("\nIngresar la opcion de su preferencia: ")
+
+    return opcion
