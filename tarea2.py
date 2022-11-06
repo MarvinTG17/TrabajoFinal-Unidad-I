ac99556be948cb989e49ef2a5d31de55f43ba27e
Luis <huanca.luis@pucp.pe>
2022-11-06 16:11:58 -0500

    correcion op 2 y 3, tarea 2

--- tarea2.py
+++ tarea2.py
@@ -13,12 +13,12 @@ while reintentarMenu:
         generacion = int(input("\nIngrese que generacion de pokemons: \nPor jemplo: 1, 2, 3, ...  "))
         obtenerPokemonesXGeneracion(generacion)
     elif op == "2":
-        lFormas = obtenerFormas(forma)
+        lFormas = obtenerFormas()
         print(f'Ingrese la forma de los pokemones: \nPor jemplo: {lFormas[0][0]}, {lFormas[1][0]} o {lFormas[2][0]}')
         forma = input()
         listarPokemonesXFormas(forma, lFormas)
     elif op == "3":
-        lHabilidades = obtenerHabilidades(habilidad)
+        lHabilidades = obtenerHabilidades()
         print(f'Ingrese la habilidad de los pokemones: \nPor jemplo: {lHabilidades[0][0]}, {lHabilidades[1][0]} o {lHabilidades[2][0]}')
         habilidad = input()
         listarPokemonesXHabilidad(habilidad, lHabilidades)
